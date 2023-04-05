d12f3afcf631323e38049e710e3310057f9cc665
Navdeep Maheshwar <navdeepmaheshwari2002@gmail.com>
2023-04-05 18:43:17 +0530

    fixing

--- app1.py
+++ app1.py
@@ -5,6 +5,7 @@ import json
 import pickle
 from fastapi import FastAPI, Response, Request
 from fastapi.middleware.cors import CORSMiddleware
+from memory_profiler import memory_usage
 app1 = FastAPI()
 
 origins = [
@@ -31,28 +32,31 @@ def home():
     return 'Movie Recommendation API '
 
 @app1.post('/recommend')
-async def recommend_movies(request: Request):
+async def recommend_movies(request:Request):
     try:
-        # movie = request.json['movie']
+        
         movi= await request.json()
         movie = movi.get('movie')
+        # movie='batman'
+        # print(movie)
         if not movie:
             return {'error': 'Please provide a movie name.'}, 400
         indices = new_data['title'][(new_data['title'].str.contains(movie))|(new_data['title'].str.lower().str.contains(movie))].index
         if len(indices) == 0:
             similar = []
             return {'recommendations': similar}
-        input = vectors.toarray()[indices[0]]
-        distances_movies, indices_movies = model.kneighbors([input])
+        input = vectors[indices[0]]
+        distances_movies, indices_movies = model.kneighbors(input)
         lst = []
         for indices in indices_movies:
             lst.extend(indices)
         similar = []
         for i in lst:
-            similar.append((new_data.iloc[i].title,str(new_data.iloc[i].id)))
+            similar.append({'title': new_data.iloc[i].title, 'id': str(new_data.iloc[i].id)})
         return {'recommendations': similar}
     except Exception as e:
         return {'error': str(e)}, 500
-
-# if __name__ == "__main__":
-#     uvicorn.run(app1, host="0.0.0.0", port=8000)
+    
+# if __name__ == '__main__':
+#     mem_usage = max(memory_usage(proc=recommend_movies))
+#     print(f"Maximum memory usage: {mem_usage} MiB")
